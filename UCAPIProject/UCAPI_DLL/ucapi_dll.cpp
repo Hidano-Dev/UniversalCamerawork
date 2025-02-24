30fe6b3dd385354d0180cf8fc00f2504f22a7306
Hidano <hidanojr@gmail.com>
2025-02-24 18:21:27 +0900

    インターフェースとなるucapi_dllを作成

---
+++ UCAPIProject/UCAPI_DLL/ucapi_dll.cpp
@@ -0,0 +1,91 @@
+#include "ucapi_dll.h"
+#include "ucapi_serialization.h"
+#include "ucapi_deserialization.h"
+#include "kaitai/kaitaistruct.h"
+#include <sstream>
+#include <vector>
+#include <cstring>
+#include <exception>
+
+// Define the wrapper structure to hold both the deserialized UCAPI object and its stream.
+struct UCAPI_DllObject {
+    ucapi_serialization_t* obj;
+    kaitai::kstream* ks;
+};
+
+/// <summary>
+/// Serializes the given UCAPI structure into a newly allocated byte buffer.
+/// </summary>
+/// <param name="ucapi">Pointer to the UCAPI structure to serialize.</param>
+/// <param name="outBuffer">Pointer to the output byte buffer pointer.</param>
+/// <param name="outSize">Pointer to the output buffer size.</param>
+/// <returns>0 on success, non-zero on failure.</returns>
+extern "C" UCAPI_API int UCAPI_Serialize(const void* ucapi, uint8_t** outBuffer, size_t* outSize) {
+    try {
+        // Cast the input pointer to the UCAPI object type.
+        const ucapi_serialization_t* ucapiObj = reinterpret_cast<const ucapi_serialization_t*>(ucapi);
+        // Create an output stream in binary mode.
+        std::ostringstream oss(std::ios::binary);
+        // Serialize the UCAPI object into the stream.
+        write_ucapi(ucapiObj, oss);
+        // Obtain the binary data.
+        std::string data = oss.str();
+        *outSize = data.size();
+        // Allocate a buffer for the output data.
+        *outBuffer = new uint8_t[*outSize];
+        std::memcpy(*outBuffer, data.data(), *outSize);
+        return 0;
+    }
+    catch (const std::exception&) {
+        return -1;
+    }
+}
+
+/// <summary>
+/// Frees a byte buffer allocated by UCAPI_Serialize.
+/// </summary>
+/// <param name="buffer">Pointer to the buffer to free.</param>
+extern "C" UCAPI_API void UCAPI_FreeBuffer(uint8_t* buffer) {
+    delete[] buffer;
+}
+
+/// <summary>
+/// Deserializes the given byte buffer into a UCAPI object.
+/// </summary>
+/// <param name="inBuffer">Pointer to the input byte buffer.</param>
+/// <param name="inSize">Size of the input buffer.</param>
+/// <returns>Pointer to a UCAPI_DllObject on success, or nullptr on failure.</returns>
+extern "C" UCAPI_API UCAPI_DllObject* UCAPI_Deserialize(const uint8_t* inBuffer, size_t inSize) {
+    try {
+        // Create a std::string from the input buffer.
+        std::string data(reinterpret_cast<const char*>(inBuffer), inSize);
+        // Create a new kaitai::kstream from the binary data.
+        kaitai::kstream* ks = new kaitai::kstream(data);
+        // Deserialize the UCAPI object using the kstream.
+        ucapi_serialization_t* obj = new ucapi_serialization_t(ks);
+        // Allocate and initialize the wrapper.
+        UCAPI_DllObject* wrapper = new UCAPI_DllObject;
+        wrapper->obj = obj;
+        wrapper->ks = ks;
+        return wrapper;
+    }
+    catch (const std::exception&) {
+        return nullptr;
+    }
+}
+
+/// <summary>
+/// Frees the UCAPI object created by UCAPI_Deserialize.
+/// </summary>
+/// <param name="obj">Pointer to the UCAPI_DllObject to free.</param>
+extern "C" UCAPI_API void UCAPI_FreeObject(UCAPI_DllObject* obj) {
+    if (obj) {
+        if (obj->obj) {
+            delete obj->obj;
+        }
+        if (obj->ks) {
+            delete obj->ks;
+        }
+        delete obj;
+    }
+}
---
+++ UCAPIProject/UCAPI_DLL/ucapi_dll.h
@@ -0,0 +1,61 @@
+#ifndef UCAPI_DLL_H
+#define UCAPI_DLL_H
+
+#ifdef _WIN32
+#ifdef UCAPI_DLL_EXPORTS
+#define UCAPI_API __declspec(dllexport)
+#else
+#define UCAPI_API __declspec(dllimport)
+#endif
+#else
+#define UCAPI_API
+#endif
+
+#include <cstddef>
+#include <cstdint>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /// <summary>
+    /// Opaque handle for a UCAPI object including its deserialized data and associated stream.
+    /// </summary>
+    typedef struct UCAPI_DllObject UCAPI_DllObject;
+
+    /// <summary>
+    /// Serializes the given UCAPI structure into a newly allocated byte buffer.
+    /// The caller must free the buffer using UCAPI_FreeBuffer.
+    /// </summary>
+    /// <param name="ucapi">Pointer to the UCAPI structure to serialize.</param>
+    /// <param name="outBuffer">Pointer to the output byte buffer pointer.</param>
+    /// <param name="outSize">Pointer to the output buffer size.</param>
+    /// <returns>0 on success, non-zero on failure.</returns>
+    UCAPI_API int UCAPI_Serialize(const void* ucapi, uint8_t** outBuffer, size_t* outSize);
+
+    /// <summary>
+    /// Frees a byte buffer allocated by UCAPI_Serialize.
+    /// </summary>
+    /// <param name="buffer">Pointer to the buffer to free.</param>
+    UCAPI_API void UCAPI_FreeBuffer(uint8_t* buffer);
+
+    /// <summary>
+    /// Deserializes the given byte buffer into a UCAPI object.
+    /// The returned handle must be freed using UCAPI_FreeObject.
+    /// </summary>
+    /// <param name="inBuffer">Pointer to the input byte buffer.</param>
+    /// <param name="inSize">Size of the input buffer.</param>
+    /// <returns>Pointer to a UCAPI_DllObject on success, or nullptr on failure.</returns>
+    UCAPI_API UCAPI_DllObject* UCAPI_Deserialize(const uint8_t* inBuffer, size_t inSize);
+
+    /// <summary>
+    /// Frees the UCAPI object created by UCAPI_Deserialize.
+    /// </summary>
+    /// <param name="obj">Pointer to the UCAPI_DllObject to free.</param>
+    UCAPI_API void UCAPI_FreeObject(UCAPI_DllObject* obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // UCAPI_DLL_H
