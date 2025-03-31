5cc5a3d3b2ca60d806185a9fece07db3941da37a
Hidano <hidanojr@gmail.com>
2025-03-31 15:04:43 +0900

    表示を整える

--- UCAPIProject/UCAPI/UCAPI_Debug/main.cpp
+++ UCAPIProject/UCAPI/UCAPI_Debug/main.cpp
@@ -50,39 +50,40 @@ int main() {
 
 
    // Recordのフィールドを表示
-   std::vector<ucapi_t::record_t*>* m_payload = obj->payload();
-   std::cout << "CameraNo" << m_payload->at(0)->camera_no() << std::endl;
-   std::cout << "Commands" << m_payload->at(0)->commands() << std::endl;
-   std::cout << "Timecode Frame Number" << m_payload->at(0)->timecode()->frame_number() << std::endl;
-   std::cout << "Timecode Second Number" << m_payload->at(0)->timecode()->second_number() << std::endl;
-   std::cout << "Timecode Minute Number" << m_payload->at(0)->timecode()->minute_number() << std::endl;
-   std::cout << "Timecode Hour Number" << m_payload->at(0)->timecode()->hour_number() << std::endl;
-   std::cout << "Timecode Reserved" << m_payload->at(0)->timecode()->reserved() << std::endl;
-   std::cout << "PacketNo" << m_payload->at(0)->packet_no() << std::endl;
-   std::cout << "Eye Position Right M" << m_payload->at(0)->eye_position_right_m() << std::endl;
-   std::cout << "Eye Position Up M" << m_payload->at(0)->eye_position_up_m() << std::endl;
-   std::cout << "Eye Position Forward M" << m_payload->at(0)->eye_position_forward_m() << std::endl;
-   std::cout << "Look Vector Right M" << m_payload->at(0)->look_vector_right_m() << std::endl;
-   std::cout << "Look Vector Up M" << m_payload->at(0)->look_vector_up_m() << std::endl;
-   std::cout << "Look Vector Forward M" << m_payload->at(0)->look_vector_forward_m() << std::endl;
-   std::cout << "Up Vector Right M" << m_payload->at(0)->up_vector_right_m() << std::endl;
-   std::cout << "Up Vector Up M" << m_payload->at(0)->up_vector_up_m() << std::endl;
-   std::cout << "Up Vector Forward M" << m_payload->at(0)->up_vector_forward_m() << std::endl;
-   std::cout << "Focal Length Mm" << m_payload->at(0)->focal_length_mm() << std::endl;
-   std::cout << "Aspect Ratio" << m_payload->at(0)->aspect_ratio() << std::endl;
-   std::cout << "Focus Distance M" << m_payload->at(0)->focus_distance_m() << std::endl;
-   std::cout << "Aperture" << m_payload->at(0)->aperture() << std::endl;
-   std::cout << "Sensor Size Width Mm" << m_payload->at(0)->sensor_size_width_mm() << std::endl;
-   std::cout << "Sensor Size Height Mm" << m_payload->at(0)->sensor_size_height_mm() << std::endl;
-   std::cout << "Near Clip M" << m_payload->at(0)->near_clip_m() << std::endl;
-   std::cout << "Far Clip M" << m_payload->at(0)->far_clip_m() << std::endl;
-   std::cout << "Lens Shift Horizontal Ratio" << m_payload->at(0)->lens_shift_horizontal_ratio() << std::endl;
-   std::cout << "Lens Shift Vertical Ratio" << m_payload->at(0)->lens_shift_vertical_ratio() << std::endl;
-   std::cout << "Lens Distortion Radial Coefficients K1" << m_payload->at(0)->lens_distortion_radial_coefficients_k1() << std::endl;
-   std::cout << "Lens Distortion Radial Coefficients K2" << m_payload->at(0)->lens_distortion_radial_coefficients_k2() << std::endl;
-   std::cout << "Lens Distortion Center Point Right Mm" << m_payload->at(0)->lens_distortion_center_point_right_mm() << std::endl;
-   std::cout << "Lens Distortion Center Point Up Mm" << m_payload->at(0)->lens_distortion_center_point_up_mm() << std::endl;
-   std::cout << "Reserved" << m_payload->at(0)->reserved() << std::endl;
+   std::vector<ucapi_t::record_t*>* m_payload = obj->m_payload;
+   std::cout << "CameraNo " << m_payload->at(0)->m_camera_no << std::endl;
+   std::cout << "Commands " << m_payload->at(0)->m_commands << std::endl;
+   std::cout << "Timecode Frame Number " << m_payload->at(0)->m_timecode->m_frame_number << std::endl;
+   std::cout << "Timecode Second Number " << m_payload->at(0)->m_timecode->m_second_number << std::endl;
+   std::cout << "Timecode Minute Number " << m_payload->at(0)->m_timecode->m_minute_number << std::endl;
+   std::cout << "Timecode Hour Number " << m_payload->at(0)->m_timecode->m_hour_number << std::endl;
+   std::cout << "Timecode Reserved " << m_payload->at(0)->m_timecode->m_reserved << std::endl;
+   std::cout << "PacketNo " << std::to_string(m_payload->at(0)->m_packet_no) << std::endl;
+   std::cout << "Eye Position Right M " << m_payload->at(0)->m_eye_position_right_m << std::endl;
+   std::cout << "Eye Position Up M " << m_payload->at(0)->m_eye_position_up_m << std::endl;
+   std::cout << "Eye Position Forward M " << m_payload->at(0)->m_eye_position_forward_m << std::endl;
+   std::cout << "Look Vector Right M " << m_payload->at(0)->m_look_vector_right_m << std::endl;
+   std::cout << "Look Vector Up M " << m_payload->at(0)->m_look_vector_up_m << std::endl;
+   std::cout << "Look Vector Forward M " << m_payload->at(0)->m_look_vector_forward_m << std::endl;
+   std::cout << "Up Vector Right M " << m_payload->at(0)->m_up_vector_right_m << std::endl;
+   std::cout << "Up Vector Up M " << m_payload->at(0)->m_up_vector_up_m << std::endl;
+   std::cout << "Up Vector Forward M " << m_payload->at(0)->m_up_vector_forward_m << std::endl;
+   std::cout << "Focal Length Mm " << m_payload->at(0)->m_focal_length_mm << std::endl;
+   std::cout << "Aspect Ratio " << m_payload->at(0)->m_aspect_ratio << std::endl;
+   std::cout << "Focus Distance M " << m_payload->at(0)->m_focus_distance_m << std::endl;
+   std::cout << "Aperture " << m_payload->at(0)->m_aperture << std::endl;
+   std::cout << "Sensor Size Width Mm " << m_payload->at(0)->m_sensor_size_width_mm << std::endl;
+   std::cout << "Sensor Size Height Mm " << m_payload->at(0)->m_sensor_size_height_mm << std::endl;
+   std::cout << "Near Clip M " << m_payload->at(0)->m_near_clip_m << std::endl;
+   std::cout << "Far Clip M " << m_payload->at(0)->m_far_clip_m << std::endl;
+   std::cout << "Lens Shift Horizontal Ratio " << m_payload->at(0)->m_lens_shift_horizontal_ratio << std::endl;
+   std::cout << "Lens Shift Vertical Ratio " << m_payload->at(0)->m_lens_shift_vertical_ratio << std::endl;
+   std::cout << "Lens Distortion Radial Coefficients K1 " << m_payload->at(0)->m_lens_distortion_radial_coefficients_k1 << std::endl;
+   std::cout << "Lens Distortion Radial Coefficients K2 " << m_payload->at(0)->m_lens_distortion_radial_coefficients_k2 << std::endl;
+   std::cout << "Lens Distortion Center Point Right Mm " << m_payload->at(0)->m_lens_distortion_center_point_right_mm << std::endl;
+   std::cout << "Lens Distortion Center Point Up Mm " << m_payload->at(0)->m_lens_distortion_center_point_up_mm << std::endl;
+   // Reservedのサイズを表示
+   std::cout << "Reserved Size " << m_payload->at(0)->m_reserved->size() << std::endl;
 
    // Free the allocated resources.
    UCAPI_FreeBuffer(serializedBuffer);
