0e8d542fa5b72bf1680f96742e248a6bd8f15327
Hidano <n.hidano@hidano-dev.com>
2026-01-17 18:42:21 +0900

    P1-2対応

--- UCAPIProject/UCAPI/UCAPI_DLL_Test/test.cpp
+++ UCAPIProject/UCAPI/UCAPI_DLL_Test/test.cpp
@@ -64,6 +64,52 @@ TEST(UcapiDll_CalcCRC16, Deterministic) {
   EXPECT_EQ(crc1, crc2);
 }
 
+// CRC16-CCITT-FALSE standard test vector: "123456789" -> 0x29B1
+// Reference: ITU-T V.41, CRC-16/CCITT-FALSE (poly=0x1021, init=0xFFFF, refIn=false, refOut=false, xorOut=0x0000)
+TEST(UcapiDll_CalcCRC16, StandardTestVector_123456789) {
+  // ASCII "123456789" = 0x31 0x32 0x33 0x34 0x35 0x36 0x37 0x38 0x39
+  uint8_t data[] = {0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
+  uint16_t crc = UCAPI_CalcCRC16(data, sizeof(data), 0x1021, 0xFFFF);
+  EXPECT_EQ(crc, 0x29B1) << "CRC16-CCITT-FALSE for '123456789' should be 0x29B1";
+}
+
+// CRC16 single byte test vectors
+TEST(UcapiDll_CalcCRC16, SingleByteTestVectors) {
+  // Single byte 0x00: init 0xFFFF, process one 0x00 byte
+  // Reference calculated value
+  uint8_t data_00[] = {0x00};
+  uint16_t crc_00 = UCAPI_CalcCRC16(data_00, sizeof(data_00), 0x1021, 0xFFFF);
+  EXPECT_EQ(crc_00, 0xE1F0) << "CRC16-CCITT-FALSE for single 0x00 should be 0xE1F0";
+
+  // Single byte 0xFF
+  uint8_t data_ff[] = {0xFF};
+  uint16_t crc_ff = UCAPI_CalcCRC16(data_ff, sizeof(data_ff), 0x1021, 0xFFFF);
+  EXPECT_EQ(crc_ff, 0xFF00) << "CRC16-CCITT-FALSE for single 0xFF should be 0xFF00";
+}
+
+// CRC16 with different polynomial (CRC-16/XMODEM: init=0x0000)
+TEST(UcapiDll_CalcCRC16, XmodemTestVector) {
+  // CRC-16/XMODEM: poly=0x1021, init=0x0000
+  // "123456789" -> 0x31C3
+  uint8_t data[] = {0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
+  uint16_t crc = UCAPI_CalcCRC16(data, sizeof(data), 0x1021, 0x0000);
+  EXPECT_EQ(crc, 0x31C3) << "CRC16-XMODEM for '123456789' should be 0x31C3";
+}
+
+// CRC16 multiple bytes all zeros
+TEST(UcapiDll_CalcCRC16, AllZerosTestVector) {
+  uint8_t data[] = {0x00, 0x00, 0x00, 0x00};
+  uint16_t crc = UCAPI_CalcCRC16(data, sizeof(data), 0x1021, 0xFFFF);
+  EXPECT_EQ(crc, 0x84C0) << "CRC16-CCITT-FALSE for four 0x00 bytes should be 0x84C0";
+}
+
+// CRC16 multiple bytes all ones (0xFF)
+TEST(UcapiDll_CalcCRC16, AllOnesTestVector) {
+  uint8_t data[] = {0xFF, 0xFF, 0xFF, 0xFF};
+  uint16_t crc = UCAPI_CalcCRC16(data, sizeof(data), 0x1021, 0xFFFF);
+  EXPECT_EQ(crc, 0x1D0F) << "CRC16-CCITT-FALSE for four 0xFF bytes should be 0x1D0F";
+}
+
 // FreeBuffer should handle nullptr without crash
 TEST(UcapiDll_FreeBuffer, Null) {
   UCAPI_FreeBuffer(nullptr);
